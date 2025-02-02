69b60aa131690c0dc0e2d2e5f8cdddfc7160ed0a
navaf <navafsoft@gmail.com>
2025-02-02 13:14:36 +0530

    feat: integrate NavBar component and enhance card grid styles

--- app/src/Components/Cards.jsx
+++ app/src/Components/Cards.jsx
@@ -1,4 +1,5 @@
 import "./card.css";
+import NavBar from "./NavBar";
 import SingleCards from "./SingleCards";
 import { useEffect, useState } from "react";
 
@@ -7,14 +8,14 @@ function Cards() {
   const [, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
-  
+
   const cardImages = [
-    { src: "/src/assets/CardDeath.jpg" , matched : false },
-    { src: "/src/assets/CardDevil.jpg" ,matched : false },
-    { src: "/src/assets/CardEmperor.jpg" ,matched : false },
-    { src: "/src/assets/CardFool.jpg" , matched : false },
-    { src: "src/assets/CardHermit.jpg", matched : false },
-    { src: "/src/assets/CardQueen.jpg" ,matched : false },
+    { src: "/src/assets/CardDeath.jpg", matched: false },
+    { src: "/src/assets/CardDevil.jpg", matched: false },
+    { src: "/src/assets/CardEmperor.jpg", matched: false },
+    { src: "/src/assets/CardFool.jpg", matched: false },
+    { src: "src/assets/CardHermit.jpg", matched: false },
+    { src: "/src/assets/CardQueen.jpg", matched: false },
   ];
 
   const shuffleCards = () => {
@@ -24,51 +25,57 @@ function Cards() {
     setCards(suffledCard);
     setTurns(0);
   };
-  
-//   handle the choices
-  const handlerChoices = (card)=>{
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
 
-//   camparing cards
-useEffect(()=>{
-if(choiceOne && choiceTwo){
-    if(choiceOne.src === choiceTwo.src){   
-        setCards(prev=>{
-          return prev.map(card =>{
-            if(card.src === choiceOne.src ){
-              return {...card, matched : true}
-            }else{
-              return card
+  const handlerChoices = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prev) => {
+          return prev.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
             }
-          })
-        })     
-        resetTurn()
-    }else{
-        setTimeout(()=>resetTurn(),1000)
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
     }
-}
-},[choiceOne,choiceTwo])
-console.log(cards)
-//   handle the turns and choice
-const resetTurn=()=>{
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(prev => prev + 1)
-}
+  }, [choiceOne, choiceTwo]);
+  console.log(cards, "hi");
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
-    <div>
+    <div className="bg-[#060621] h-screen">
+      <div className="fixed top-0 w-full  left-0">
+        <NavBar />
+      </div>
       <div className="text-center">
         <h1 className="text-[30px] font-[600] text-white">Magic</h1>
-        <button onClick={shuffleCards} className="text-[30px] font-600 text-white">New game</button>
       </div>
       <div className="card-grid">
-        {cards.map((card) => {
-          return <SingleCards handlerChoices={handlerChoices} key={card.id} card={card}
-          flipped = {card === choiceOne || card === choiceTwo || card.matched}
-          />;
-        })}
+        {cards.map((card) => (
+          <SingleCards
+            handlerChoices={handlerChoices}
+            key={card.id}
+            card={card}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
+        ))}
       </div>
     </div>
   );
