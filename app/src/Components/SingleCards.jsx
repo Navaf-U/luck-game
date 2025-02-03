91eea794afea707ddcec6d1ff2fc0fdcb4b80475
navaf <navafsoft@gmail.com>
2025-02-03 14:08:05 +0530

    feat: update cover card image import in SingleCards component

--- app/src/Components/SingleCards.jsx
+++ app/src/Components/SingleCards.jsx
@@ -1,6 +1,6 @@
 import './singleCard.css'
 /* eslint-disable react/prop-types */
-
+import coverPhoto from '../assets/coverCard.jpg'
 function SingleCards( { card , handlerChoices , flipped }) {
     const handleClick =()=>{
         handlerChoices(card)
@@ -9,7 +9,7 @@ function SingleCards( { card , handlerChoices , flipped }) {
     <div className='card'>
       <div className={flipped ? "flipped" : ""}>
         <img src={card.src} className='front h-full object-cover' alt="card front" />                    
-        <img src="/src/assets/coverCard.jpg" className='back h-full ' alt="card back" onClick={handleClick} />
+        <img src={coverPhoto} className='back h-full ' alt="card back" onClick={handleClick} />
       </div>
 </div>
   )
