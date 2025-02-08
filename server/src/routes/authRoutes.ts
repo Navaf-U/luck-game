27c2d21c47bb64cadca04ecefd47f1029f1b712b
navaf <navafsoft@gmail.com>
2025-02-08 12:08:50 +0530

    Recalculate GitHub linguist stats

--- server/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express  from "express";
-import trycatch from "../utils/trycatch";
-import { login , register } from "../controllers/auth/authController";
-const router = express.Router();
-
-router
-.post("/register",trycatch(register))
-.post("/login",trycatch(login))
-export default router
