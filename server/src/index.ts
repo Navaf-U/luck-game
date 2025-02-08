2981b157cb399ae45c5e17ad82d478a42b793e02
navaf <navafsoft@gmail.com>
2025-02-08 11:17:03 +0530

    feat: add custom error handling and try-catch utility; update server to use environment port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 const app = express();
 dotenv.config();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 
 
-app.listen(3000, () => {
-    console.log("Connected to backend");
+interface Error {
+    status?: number;
+    message?: string;
+}
+
+app.listen(PORT, () => {
+    console.log("Connected to server " + PORT);
 });
---
+++ server/src/utils/trycatch.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+const trycatch = (func: (req: Request, res: Response, next: NextFunction) => Promise<void>) => async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        await func(req,res,next)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default trycatch;
